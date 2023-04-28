28f728dffce2d6f9d4958ebf9c81df98bc0ba4ee
mihai10001 <rvivits@gmail.com>
2023-04-28 14:43:20 +0300

    cleanup boilerplate (remove unused imports, comments, namespaces)

--- app/Amach.API/Startup.cs
+++ app/Amach.API/Startup.cs
@@ -3,25 +3,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Amach.API
+namespace Amach.API;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-        }
+    }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseRouting();
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-        }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
 }
