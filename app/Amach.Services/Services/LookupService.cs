92a4a1fc7a68be253cb1a9c0cc4af0dbedacc3a6
mihai10001 <rvivits@gmail.com>
2023-04-28 16:22:14 +0300

    add CreditDataAPIOptions in LookupService

--- app/Amach.Services/Services/LookupService.cs
+++ app/Amach.Services/Services/LookupService.cs
@@ -1,11 +1,20 @@
 using System;
 using Amach.Domain.Interfaces;
 using Amach.Domain.Models;
+using Amach.Domain.Options;
+using Microsoft.Extensions.Options;
 
 namespace Amach.Services.Services;
 
 public class LookupService : ILookupService
 {
+    private readonly CreditDataAPIOptions _creditDataAPIOptions;
+
+    public LookupService(IOptions<CreditDataAPIOptions> options)
+    {
+        _creditDataAPIOptions = options.Value;
+    }
+
     public CreditData Lookup(string SSN)
     {
         throw new NotImplementedException();
