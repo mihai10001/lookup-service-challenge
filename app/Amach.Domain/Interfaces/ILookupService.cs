1b54864dcbaaf0208a266aa097a9c09e6107f627
mihai10001 <rvivits@gmail.com>
2023-04-28 15:30:51 +0300

    create LookupService structure

---
+++ app/Amach.Domain/Interfaces/ILookupService.cs
@@ -0,0 +1,8 @@
+﻿using Amach.Domain.Models;
+
+namespace Amach.Domain.Interfaces;
+
+public interface ILookupService
+{
+    public CreditData Lookup(string SSN);
+}
---
+++ app/Amach.Services/Services/LookupService.cs
@@ -0,0 +1,13 @@
+using System;
+using Amach.Domain.Interfaces;
+using Amach.Domain.Models;
+
+namespace Amach.Services.Services;
+
+public class LookupService : ILookupService
+{
+    public CreditData Lookup(string SSN)
+    {
+        throw new NotImplementedException();
+    }
+}
