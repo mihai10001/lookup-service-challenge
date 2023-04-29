e59801386d01f5096c91142f623e481d29f93ca7
mihai10001 <rvivits@gmail.com>
2023-04-29 13:39:52 +0300

    models to read from CreditDataAPI

---
+++ app/Amach.Domain/Models/DebtDetails.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Amach.Domain.Models;
+
+public class DebtDetails
+{
+    [JsonPropertyName("balance_of_debt")]
+    public int BalanceOfDebt { get; set; }
+
+    [JsonPropertyName("complaints")]
+    public bool Complaints { get; set; }
+}
