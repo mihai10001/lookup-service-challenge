0e1b0afc345309d788608d549a67ee7deacb52c4
mihai10001 <rvivits@gmail.com>
2023-04-29 13:48:37 +0300

    Lookup controller with endpoint for credit data

---
+++ app/Amach.API/Controllers/LookupController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Amach.Domain.Interfaces;
+using Amach.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amach.API.Controllers;
+
+[ApiController]
+public class LookupController : Controller
+{
+    private readonly ILookupService _lookupService;
+
+    public LookupController(ILookupService lookupService) => _lookupService = lookupService;
+
+    [HttpGet]
+    [Route("credit-data/{ssn}")]
+    public async Task<ActionResult<CreditData>> CreditData(string SSN)
+    {
+        return await _lookupService.Lookup(SSN);
+    }
+}
