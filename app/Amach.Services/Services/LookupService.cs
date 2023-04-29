e89610ae3bfc55a78ef7f6b3c8c40ab9e87482ad
mihai10001 <rvivits@gmail.com>
2023-04-29 13:42:20 +0300

    methods to read from CreditDataAPI using Flurl

--- app/Amach.Services/Services/LookupService.cs
+++ app/Amach.Services/Services/LookupService.cs
@@ -1,4 +1,6 @@
-using System;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
 using Amach.Domain.Interfaces;
 using Amach.Domain.Models;
 using Amach.Domain.Options;
@@ -18,5 +20,32 @@ public class LookupService : ILookupService
     public CreditData Lookup(string SSN)
     {
         throw new NotImplementedException();
+    }
+
+    private Task<PersonalDetails> GetPersonalDetails(string SSN)
+    {
+        return _creditDataAPIOptions
+            .BaseUrl
+            .AppendPathSegment("personal-details")
+            .AppendPathSegment(SSN)
+            .GetJsonAsync<PersonalDetails>();
+    }
+
+    private Task<AssessedIncomeDetails> GetAssessedIncomeDetails(string SSN)
+    {
+        return _creditDataAPIOptions
+            .BaseUrl
+            .AppendPathSegment("assessed-income")
+            .AppendPathSegment(SSN)
+            .GetJsonAsync<AssessedIncomeDetails>();
+    }
+
+    private Task<DebtDetails> GetDebtDetails(string SSN)
+    {
+        return _creditDataAPIOptions
+            .BaseUrl
+            .AppendPathSegment("debt")
+            .AppendPathSegment(SSN)
+            .GetJsonAsync<DebtDetails>();
     }
 }
